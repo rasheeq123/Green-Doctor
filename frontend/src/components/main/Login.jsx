faf73c8cbdeea0ec034b8ff0d9a9378eec128576
Omkar Sharma <omkarsharma2821@gmail.com>
2023-11-26 12:41:21 +0530

    links added

--- frontend/src/components/main/Login.jsx
+++ frontend/src/components/main/Login.jsx
@@ -1,32 +1,44 @@
-import { Button, Card, CardContent, Grid, Paper, TextField } from '@mui/material'
-import React from 'react'
+import { Box, Button, Card, CardContent, Grid, InputAdornment, Paper, TextField } from '@mui/material'
+import React from 'react';
+import { AccountCircle } from '@mui/icons-material';
 
 const Login = () => {
   return (
     <div >
 
-            <h2 className="my-3"><center>Login Form</center></h2>
-            <Paper sx={{boxShadow:0}}>
-              <Grid container>
-                <Grid item md={4} sx={{mx: 'auto'}}>
-                  <Card sx={{
-                    height: 250, bgcolor: 'background.card',
-                    boxShadow: 5,
-                    borderRadius: 1,
-                    p: 4
-                  }}>
-                    <CardContent>
-                      <TextField required label="Email Address" variant='outlined' color='error' fullWidth sx={{mt:2}}/>
-                      <TextField required type="password" label="Password" variant='outlined' color='error'  fullWidth sx={{mt:2}} />
-                      <Button variant="contained" disableElevation sx={{mt:3}} >
-                        Login
-                      </Button>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              </Grid>
-            </Paper>
-          </div>
+      <h2 className="my-3"><center>Login Form</center></h2>
+      <Paper sx={{ boxShadow: 0 }}>
+        <Grid container>
+          <Grid item md={4} sx={{ mx: 'auto' }}>
+            <Card sx={{
+              height: 250, bgcolor: 'background.card',
+              boxShadow: 5,
+              borderRadius: 1,
+              p: 4
+            }}>
+              <CardContent>
+                <TextField
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="start" >
+                        <AccountCircle />
+                      </InputAdornment>
+                    )
+                  }}
+                  required label="Email Address" variant='outlined' color='error' fullWidth sx={{ mt: 2 }} />
+
+                <TextField required type="password" label="Password" variant='outlined' color='error' fullWidth sx={{ mt: 2 }} />
+                <Box sx={{ textAlign: 'center' }}>
+                  <Button variant="contained" disableElevation sx={{ mt: 3}} fullWidth >
+                    Login
+                  </Button>
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div >
   )
 }
 export default Login;
--- frontend/src/components/main/Navbar.jsx
+++ frontend/src/components/main/Navbar.jsx
@@ -12,13 +12,22 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 const pages = ['Home', 'Admin', 'User', 'Expert','Signup', 'Login'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = [{
+  text: 'Profile',
+  link : '/user/profile'
+},
+{
+  text: 'Dashboard',
+  link : '/user/dashboard'
+}];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const navigate = useNavigate();
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -148,8 +157,8 @@ function ResponsiveAppBar() {
               onClose={handleCloseUserMenu}
             >
               {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
+                <MenuItem key={setting.text} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center" onClick={() => navigate(setting.link)} >{setting.text}</Typography>
                 </MenuItem>
               ))}
             </Menu>
