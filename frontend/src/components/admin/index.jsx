7db11789a39eb8d0c3ce20975bcb366d41b40de0
Omkar Sharma <omkarsharma2821@gmail.com>
2024-01-07 17:27:43 +0530

    added footer in every page

--- frontend/src/components/admin/index.jsx
+++ frontend/src/components/admin/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { Outlet } from 'react-router-dom'
+import Footer from '../main/Footer'
 
 const Admin = () => {
   return (
     <>
     <Navbar />
     <Outlet />
+    <Footer/>
     </>
   )
 }
--- frontend/src/components/user/index.jsx
+++ frontend/src/components/user/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Prediction from './Prediction'
+import Footer from '../main/Footer'
 
 const User = () => {
   return (
     <>
     <Navbar/>
     <Prediction/>
+    <Footer/>
     <Outlet/>
     </>
   )
