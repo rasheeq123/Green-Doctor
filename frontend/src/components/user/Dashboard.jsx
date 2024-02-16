24c1b46802705b593b5c97615a2faadd725df5be
Omkar Sharma <omkarsharma2821@gmail.com>
2024-02-17 00:07:00 +0530

    merge

--- frontend/src/components/user/Dashboard.jsx
+++ frontend/src/components/user/Dashboard.jsx
@@ -82,7 +82,7 @@ const Prediction = () => {
               backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/images/fruit.jpg')`,
               flexDirection: "column",
               justifyContent: "center",
-              alignItems: "center",
+              alignItems: "center",  
               borderRadius: 3,
               minHeight: 300,
               width: "100%",
