835fb8d2734cd34689ff6f6684cdfe69062b85e5
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-02-18 00:42:49 +0530

    fix: flower card

--- frontend/src/components/user/Dashboard.jsx
+++ frontend/src/components/user/Dashboard.jsx
@@ -5,12 +5,12 @@ import { ToastContainer, toast } from "react-toastify";
 
 const theme = createTheme();
 
-const StyledPaper = styled (Paper)({
+const StyledPaper = styled(Paper)({
   padding: theme.spacing(0),
   border: '1 px solid black',
   transition: 'transform 0.3s ease-in-out',
   '&:hover': {
-      transform: 'scale(1.1)', 
+    transform: 'scale(1.1)',
   },
 
   '& img': {
@@ -33,143 +33,151 @@ const Prediction = () => {
       closeOnClick: true,
       pauseOnHover: true,
       draggable: true,
-  });
+    });
   };
   return (
     <div style={{ marginTop: 50 }}>
       <Typography variant="h4" align="center" gutterBottom mt={15}>
         Select the category for prediction
       </Typography>
-       <ToastContainer/>
-      <Container sx={{mb:5, mt:5}}>
-      <Grid container spacing={4} justifyContent="center">
-        <Grid item xs={12} md={3}>
-        <StyledPaper>
-          <Container
-            sx={{
-              display: "flex",
-              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/images/flower.jpg')`,
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: 2,
-              minHeight: 300,
-              width: "100%",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-            >
-            <Button
-              onClick={() => handleCategorySelect("flower")}
-              // sx={{mt:29}}
-              component={Link}
-              to="/user/prediction/flower"
-              variant="contained"
-              fullWidth
-              size="large"
-              // color="primary"
-              style={{ color: 'primary', backgroundColor: 'transparent' }}
-              
-            >
-              Flower
-            </Button>
-          </Container>
-          </StyledPaper>
+      <ToastContainer />
+      <Container sx={{ mb: 5, mt: 5 }}>
+        <Grid container spacing={4} justifyContent="center">
+          <Grid item xs={12} md={3}>
+            <StyledPaper>
+              <Container
+                sx={{
+                  display: "flex",
+                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/images/flower.jpg')`,
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 2,
+                  minHeight: 300,
+                  width: "100%",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              >
+                <Button
+                  onClick={() => handleCategorySelect("flower")}
+                  // sx={{mt:29}}
+                  component={Link}
+                  to="/user/prediction/flower"
+                  variant="contained"
+                  fullWidth
+                  size="medium"
+                  style={{
+                    color: 'black',
+                    fontWeight: 'bold',
+                    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                    border: '2px solid transparent', // Initial border style
+                    transition: 'border-color 0.3s ease', // Smooth transition for border color change
+                  }}
+                  // Add hover effect
+                  onMouseOver={(event) => event.target.style.borderColor = 'white'}
+                  onMouseOut={(event) => event.target.style.borderColor = 'transparent'}
+
+                >
+                  Flower
+                </Button>
+              </Container>
+            </StyledPaper>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <StyledPaper>
+              <Container
+                sx={{
+                  display: "flex",
+                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/images/fruit.jpg')`,
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 3,
+                  minHeight: 300,
+                  width: "100%",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              >
+                <Button
+                  onClick={() => handleCategorySelect("fruit")}
+                  component={Link}
+                  to="/user/prediction/fruit"
+                  variant="contained"
+                  fullWidth
+                  size="large"
+                  color="primary"
+                >
+                  Fruit
+                </Button>
+              </Container>
+            </StyledPaper>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <StyledPaper>
+              <Container
+                sx={{
+                  display: "flex",
+                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/images/veg.jpg')`,
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 3,
+                  minHeight: 300,
+                  width: "100%",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              >
+                <Button
+                  onClick={() => handleCategorySelect("vegetable")}
+                  component={Link}
+                  to="/user/prediction/vegetable"
+                  variant="contained"
+                  fullWidth
+                  size="large"
+                  color="primary"
+                >
+                  Vegetable
+                </Button >
+              </Container>
+            </StyledPaper>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <StyledPaper>
+              <Container
+                sx={{
+                  display: "flex",
+                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/images/crop.jpg')`,
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 2,
+                  minHeight: 300,
+                  width: "100%",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              >
+                <Button
+                  onClick={() => handleCategorySelect("crops")}
+                  component={Link}
+                  to="/user/prediction/crops"
+                  variant="contained"
+                  fullWidth
+                  size="large"
+                  color="primary"
+                >
+                  Crop
+                </Button>
+              </Container>
+            </StyledPaper>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={3}>
-          <StyledPaper>
-          <Container
-            sx={{
-              display: "flex",
-              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/images/fruit.jpg')`,
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",  
-              borderRadius: 3,
-              minHeight: 300,
-              width: "100%",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          > 
-            <Button
-              onClick={() => handleCategorySelect("fruit")}
-              component={Link}
-              to="/user/prediction/fruit"
-              variant="contained"
-              fullWidth
-              size="large"
-              color="primary"
-            >
-              Fruit
-            </Button>
-          </Container>
-          </StyledPaper>
-        </Grid>
-        <Grid item xs={12} md={3}>
-        <StyledPaper>
-          <Container
-            sx={{
-              display: "flex",
-              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/images/veg.jpg')`,
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: 3,
-              minHeight: 300,
-              width: "100%",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          >
-            <Button
-            onClick={( ) => handleCategorySelect( "vegetable" )}
-              component={Link}
-              to="/user/prediction/vegetable"
-              variant="contained"
-              fullWidth
-              size="large"
-              color="primary"
-            >
-              Vegetable
-            </Button >
-          </Container>
-          </StyledPaper>
-        </Grid>
-        <Grid item xs={12} md={3}>
-        <StyledPaper>
-          <Container
-            sx={{
-              display: "flex",
-              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/images/crop.jpg')`,
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: 2,
-              minHeight: 300,
-              width: "100%",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          >
-            <Button
-              onClick={() => handleCategorySelect("crops")}
-              component={Link}
-              to="/user/prediction/crops"
-              variant="contained"
-              fullWidth
-              size="large"
-              color="primary"
-            >
-              Crop
-            </Button>
-          </Container>
-          </StyledPaper>
-        </Grid>
-      </Grid>
       </Container>
     </div>
   );
 };
- 
+
 export default Prediction;
