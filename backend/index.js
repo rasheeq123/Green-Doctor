1604b141d4eca0c661bc4dbde6c76a1e7419ac56
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-02-19 20:45:39 +0530

    implement: history

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const orderRouter=require('./routers/orderRouter');
 const imageRouter=require('./routers/imageRouter');
 const expertRouter=require('./routers/expertRouter');
 const utilRouter=require('./routers/util');
-const historyRouter=require('./routers/history');
+const historyRouter=require('./routers/historyRouter');
 
 
 const cors = require('cors');
