71cc60bd485428cbb4d883bca714c4e549783bcf
Omkar Sharma <omkarsharma2821@gmail.com>
2024-02-24 17:46:00 +0530

    Add: input fields

--- frontend/src/components/main/ResetPassword.jsx
+++ frontend/src/components/main/ResetPassword.jsx
@@ -81,83 +81,119 @@ const ResetPassword = () => {
 
   return (
     <>
-    <ToastContainer />
-    <Box sx={{backgroundImage:
-      "radial-gradient(" +
-      "circle at 20% 100%, " +
-      "rgba(184, 184, 184, 0.1) 0%, " +
-      "rgba(184, 184, 184, 0.1) 33%, " +
-      "rgba(96, 96, 96, 0.1) 33%, " +
-      "rgba(96, 96, 96, 0.1) 66%, " +
-      "rgba(7, 7, 7, 0.1) 66%, " +
-      "rgba(7, 7, 7, 0.1) 99%" +
-      "), " +
-      "linear-gradient(40deg, #040a22, #162561, #202e64, #6f7aa6)",height:'95vh'}}>
-      <Box sx={{ mb: 15, }}>
-        <Grid container>
-          <Grid item md={4} sx={{ mx: "auto" }}>
-            <Card
-              sx={{
-                height: 400,
-                boxShadow: 5,
-                borderRadius: 3,
-                p: 4,
-                mt:20
-              }}
-            >
-              <Typography variant="h4" sx={{ textAlign: "center", }}>
-                Reset your password
-              </Typography>
-              <CardContent>
-                <form>
-                  <TextField
-                    id="email"
-                    ref={emailRef}
-                    required
-                    type="email"
-                    label="Enter Registered Email"
-                    fullWidth
-                    variant="outlined"
-                    color="success"
-                    sx={{ mt: 2 }}
-                  />
-                  <Button
-                    fullWidth
-                    variant="contained"
-                    color="success"
-                    sx={{ mt: 2, borderRadius: 5 }}
-                    onClick={sendOTP}
-                  >
-                    Send OTP
-                  </Button>
-                  <TextField
-                    id="text"
-                    ref={otpRef}
-                    required
-                    type="text"
-                    label="Enter OTP"
-                    fullWidth
-                    variant="outlined"
-                    color="success"
-                    sx={{ mt: 2 }}
-                  />
-                  <Button
-                    fullWidth
-                    variant="contained"
-                    color="success"
-                    sx={{ mt: 2, borderRadius: 5 }}
-                    onClick={verifyOTP}
-                  >
-                    verify otp
-                  </Button>
-                </form>
-              </CardContent>
-            </Card>
+      <ToastContainer />
+      <Box
+        sx={{
+          backgroundImage:
+            "radial-gradient(" +
+            "circle at 20% 100%, " +
+            "rgba(184, 184, 184, 0.1) 0%, " +
+            "rgba(184, 184, 184, 0.1) 33%, " +
+            "rgba(96, 96, 96, 0.1) 33%, " +
+            "rgba(96, 96, 96, 0.1) 66%, " +
+            "rgba(7, 7, 7, 0.1) 66%, " +
+            "rgba(7, 7, 7, 0.1) 99%" +
+            "), " +
+            "linear-gradient(40deg, #040a22, #162561, #202e64, #6f7aa6)",
+          height: "105vh",
+        }}
+      >
+        <Box sx={{ mb: 15 }}>
+          <Grid container>
+            <Grid item md={4} sx={{ mx: "auto" }}>
+              <Card
+                sx={{
+                  height: 550,
+                  boxShadow: 5,
+                  borderRadius: 3,
+                  p: 2,
+                  mt: 15,
+                }}
+              >
+                <Typography variant="h4" sx={{ textAlign: "center" }}>
+                  Reset your password
+                </Typography>
+                <CardContent>
+                  <form>
+                    <TextField
+                      id="email"
+                      ref={emailRef}
+                      required
+                      type="email"
+                      label="Enter Registered Email"
+                      fullWidth
+                      variant="outlined"
+                      color="success"
+                      sx={{ mt: 0 }}
+                    />
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      color="success"
+                      sx={{ mt: 2, borderRadius: 5 }}
+                      onClick={sendOTP}
+                    >
+                      Send OTP
+                    </Button>
+                    <TextField
+                      id="text"
+                      ref={otpRef}
+                      required
+                      type="text"
+                      label="Enter OTP"
+                      fullWidth
+                      variant="outlined"
+                      color="success"
+                      sx={{ mt: 2 }}
+                    />
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      color="success"
+                      sx={{ mt: 2, borderRadius: 5 }}
+                      onClick={verifyOTP}
+                    >
+                      verify otp
+                    </Button>
+                    <TextField
+                      id="text"
+                      ref={otpRef}
+                      required
+                      type="text"
+                      label="Enter new password"
+                      fullWidth
+                      variant="outlined"
+                      color="success"
+                      sx={{ mt: 2 }}
+                    />
+                    <TextField
+                      id="text"
+                      ref={otpRef}
+                      required
+                      type="text"
+                      label="Confirm new password"
+                      fullWidth
+                      variant="outlined"
+                      color="success"
+                      sx={{ mt: 2 }}
+                    />
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      color="success"
+                      sx={{ mt: 2, borderRadius: 5 }}
+                      // onClick={verifyOTP}
+                    >
+                      Reset Password
+                    </Button>
+                  </form>
+                </CardContent>
+              </Card>
+            </Grid>
           </Grid>
-        </Grid>
+        </Box>
       </Box>
-      </Box>
-      </>
+    </>
   );
 };
 
