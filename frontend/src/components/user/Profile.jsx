44b710027d4bff37fddd1ac51f0a96d522258ad2
Omkar Sharma <omkarsharma2821@gmail.com>
2024-04-01 21:42:50 +0530

    Add chart.js and react-chartjs-2 dependencies, and update Profile component

--- frontend/src/components/user/Profile.jsx
+++ frontend/src/components/user/Profile.jsx
@@ -1,11 +1,79 @@
-import { Avatar, Button, Container, Grid, Paper, Typography } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Container,
+  Grid,
+  Paper,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { NavLink } from "react-router-dom";
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  ArcElement,
+} from "chart.js";
+import { Bar, Line, Pie } from "react-chartjs-2";
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  ArcElement
+);
+const chartData = {
+  labels: [
+    "Fruit",
+    "Crop",
+    "Flower",
+    "Vegetable",
+    "Flower",
+    "Crop",
+    "Vegetable",
+  ],
+  datasets: [
+    {
+      label: "No  of Predictions",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+        "rgba(255, 205, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(201, 203, 207, 0.2)",
+      ],
+      borderColor: [
+        "rgb(255, 99, 132)",
+        "rgb(255, 159, 64)",
+        "rgb(255, 205, 86)",
+        "rgb(75, 192, 192)",
+        "rgb(54, 162, 235)",
+        "rgb(153, 102, 255)",
+        "rgb(201, 203, 207)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 
 const Profile = () => {
   return (
     <>
-      <Container sx={{ mt: 15, mb: 12 }}>
+      <Container sx={{ mt: 15, mb: 15 }}>
         <Grid container spacing={2} style={{ height: "500px" }}>
           <Grid item xs={12} sm={6} md={3}>
             <Paper
@@ -43,9 +111,14 @@ const Profile = () => {
                 overflow: "hidden",
               }}
             >
-              <Typography variant="h3" mb={2} mt={20} fontSize="1.6rem">
+              {/* <Typography variant="h3" mb={2} mt={20} fontSize="1.6rem">
                 Diagnosed graph here
-              </Typography>
+              </Typography> */}
+              <div className="container">
+                <Bar data={chartData} />
+                <hr />
+                <Line data={chartData} />
+              </div>
             </Paper>
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
@@ -72,10 +145,54 @@ const Profile = () => {
               >
                 Grow with Confidence
               </Typography>
-              <NavLink to="/user/dashboard"><Button variant="contained" color="primary" sx={{mt:1, mb: 1,textTransform: "none",fontSize: "18px", borderRadius:5,width: "100%",}}> Go to prediction page</Button></NavLink>
-              <NavLink to="/user/history"><Button variant="contained" color="primary" sx={{mb: 1,textTransform: "none",fontSize: "18px", borderRadius:5,width: "100%"}}>Detailed Diagnosis</Button></NavLink>
-              <NavLink to="/main/home"><Button variant="contained" color="primary" sx={{mb: 1,textTransform: "none",fontSize: "18px", borderRadius:5,width: "100%"}}> Return to home page</Button></NavLink>
-              
+              <NavLink to="/user/dashboard">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{
+                    mt: 1,
+                    mb: 1,
+                    textTransform: "none",
+                    fontSize: "18px",
+                    borderRadius: 5,
+                    width: "100%",
+                  }}
+                >
+                  {" "}
+                  Go to prediction page
+                </Button>
+              </NavLink>
+              <NavLink to="/user/history">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{
+                    mb: 1,
+                    textTransform: "none",
+                    fontSize: "18px",
+                    borderRadius: 5,
+                    width: "100%",
+                  }}
+                >
+                  Detailed Diagnosis
+                </Button>
+              </NavLink>
+              <NavLink to="/main/home">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{
+                    mb: 1,
+                    textTransform: "none",
+                    fontSize: "18px",
+                    borderRadius: 5,
+                    width: "100%",
+                  }}
+                >
+                  {" "}
+                  Return to home page
+                </Button>
+              </NavLink>
             </Paper>
           </Grid>
         </Grid>
