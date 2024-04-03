2b8327508d04966f73c6cc78cac885240a61f597
Omkar Sharma <omkarsharma2821@gmail.com>
2024-04-03 23:28:31 +0530

    Remove unused imports and refactor dark mode toggle

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Login from "./components/main/Login";
 import Signup from "./components/main/Signup";
 import User from "./components/user";
 import Profile from "./components/user/Profile";
-import { Switch, ThemeProvider, createTheme } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material";
 import Admin from "./components/admin";
 import ManageUser from "./components/admin/ManageUser";
 import ResetPassword from "./components/main/ResetPassword";
@@ -17,41 +17,17 @@ import { AppProvider } from "./context/AppContext";
 import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { useState } from "react";
-import { DarkMode } from "@mui/icons-material";
 
 
 function App() {
   const {user , loginWithRedirect } = useAuth0();
   console.log("current user : " ,user);
-  // const theme = createTheme({});
-  // state to manage the dark mode
-  const [toggleDarkMode, setToggleDarkMode] = useState(true);
+  const theme = createTheme({});
 
-  // function to toggle the dark mode as true or false
-  const toggleDarkTheme = () => {
-    setToggleDarkMode(!toggleDarkMode);
-  };
-
-  // applying the primary and secondary theme colors
-  const darkTheme = createTheme({
-    palette: {
-      mode: toggleDarkMode ? 'dark' : 'light', // handle the dark mode state on toggle
-      primary: {
-        main: '#90caf9',
-      },
-      secondary: {
-        main: '#131052',
-
-      },
-    },
-  });
-  
 
   return (
     <>
-      <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-        <Switch checked={toggleDarkMode} onChange={toggleDarkTheme} />
+      <ThemeProvider theme={theme}>
         <BrowserRouter>
         <Auth0Provider
         domain="dev-spgtplnzvs7tyejk.us.auth0.com"
